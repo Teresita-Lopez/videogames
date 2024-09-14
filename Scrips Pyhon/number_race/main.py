2aa1da6e0529b3843c776580b1ee2cc136e8380c
Teresita <silvanalop07@gmail.com>
2024-09-13 21:31:25 -0500

    message

---
+++ Scrips Pyhon/number_race/main.py
@@ -0,0 +1,13 @@
+'''Scrips description: Number race
+dev: Teresita López
+date: 13/09/24
+'''
+
+from random import randint 
+
+dice1 = randint (1,6)
+dice2 = randint (1,6) 
+
+
+print (f"Dice 1: {dice1}")
+print (f"Dice 2: {dice2}")
